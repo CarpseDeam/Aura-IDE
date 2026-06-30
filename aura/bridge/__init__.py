62afb10bef9cf92efaeacaa35073267b6652c538
CarpseDeam <meadk989@gmail.com>
2026-06-30 11:23:45 -0400

    Install dispatch session bridge

--- aura/bridge/__init__.py
+++ aura/bridge/__init__.py
@@ -1,5 +1,9 @@
 """Adapters bridging the synchronous ConversationManager to Qt signals."""
+from aura.bridge.dispatch import _DispatchProxy
+from aura.bridge.dispatch_session import install_dispatch_session_bridge
 from aura.bridge.lap_result import LapResult
 from aura.bridge.qt_bridge import ConversationBridge
+
+install_dispatch_session_bridge(_DispatchProxy)
 
 __all__ = ["ConversationBridge", "LapResult"]
